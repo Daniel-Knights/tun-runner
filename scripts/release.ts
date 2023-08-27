b078f146872f1e8e6fb1b40ff7f6a7701af8d631
Daniel Knights <daniel.knights@28b.co.uk>
2023-08-27 12:57:30 +0100

    ci(release): use spawn, fix changenog call

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,19 +1,23 @@
-import { execFileSync } from "node:child_process";
+import { spawnSync } from "node:child_process";
 import fs from "node:fs";
 
 const args = process.argv.slice(2);
 
 fs.rmSync("dist", { recursive: true, force: true });
 
-execFileSync("pnpm", ["run", "checks"]);
-execFileSync("pnpm", ["build"]);
-execFileSync("pnpm", ["version", args[0]]);
-execFileSync("changenog");
-execFileSync("git", ["add", "."]);
+function exec(cmd: string, passedArgs: string[]): void {
+  spawnSync(cmd, passedArgs, { stdio: "inherit" });
+}
+
+exec("pnpm", ["run", "checks"]);
+exec("pnpm", ["build"]);
+exec("pnpm", ["version", args[0]]);
+exec("npx", ["changenog"]);
+exec("git", ["add", "."]);
 
 const pkg = await import("../package.json");
 
-execFileSync("git", ["commit", "-m", `docs(changelog): v${pkg.default.version}`]);
-execFileSync("git", ["push"]);
-execFileSync("git", ["push", "--tags"]);
-execFileSync("pnpm", ["publish"]);
+exec("git", ["commit", "-m", `docs(changelog): v${pkg.default.version}`]);
+exec("git", ["push"]);
+exec("git", ["push", "--tags"]);
+exec("pnpm", ["publish"]);
